57ef0afc4a22d00598eebc312cc89af344e4cf19
Haranmes <soliman.ramez@proton.me>
2025-06-26 14:44:34 +0200

    test

---
+++ target/debug/deps/tremio-837eecd817d9eabf.d
@@ -0,0 +1,5 @@
+/home/rsoliman/Work/tremio/target/debug/deps/tremio-837eecd817d9eabf.d: src/main.rs
+
+/home/rsoliman/Work/tremio/target/debug/deps/libtremio-837eecd817d9eabf.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/tremio-ac7e401f8a4c4080.d
@@ -0,0 +1,5 @@
+/home/rsoliman/Work/tremio/target/debug/deps/tremio-ac7e401f8a4c4080.d: src/main.rs
+
+/home/rsoliman/Work/tremio/target/debug/deps/libtremio-ac7e401f8a4c4080.rmeta: src/main.rs
+
+src/main.rs:
