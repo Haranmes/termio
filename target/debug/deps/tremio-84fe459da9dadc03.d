d2d3fa3ad345386eeacfbe70c6e64639dd6777d3
Viktor Popp <viktorpopp.dev@gmail.com>
2025-06-27 10:47:10 +0200

    Deleted target folder

--- target/debug/deps/tremio-84fe459da9dadc03.d
+++
@@ -1,7 +0,0 @@
-/home/rsoliman/Work/tremio/target/debug/deps/tremio-84fe459da9dadc03.d: src/lib.rs src/commands/mod.rs src/commands/commands_enum.rs
-
-/home/rsoliman/Work/tremio/target/debug/deps/libtremio-84fe459da9dadc03.rmeta: src/lib.rs src/commands/mod.rs src/commands/commands_enum.rs
-
-src/lib.rs:
-src/commands/mod.rs:
-src/commands/commands_enum.rs:
--- target/debug/deps/tremio-a6c1882dafe4857f.d
+++
@@ -1,5 +0,0 @@
-/home/rsoliman/Work/tremio/target/debug/deps/tremio-a6c1882dafe4857f.d: src/main.rs
-
-/home/rsoliman/Work/tremio/target/debug/deps/libtremio-a6c1882dafe4857f.rmeta: src/main.rs
-
-src/main.rs:
